caa714c8d426362776587e074f9374da6929edf0
Priyesh V <priyeshathiyan@gmail.com>
2024-07-02 14:53:18 +0000

    adding shell script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#read -p "Enter Build Version:" $build_version 
+
+# Navigate to the project directory
+cd /root/app/reactjs-build/
+
+# Build the Docker image
+docker build -t priyeshshinav/public-dev-guvi:$1 .
+
+# Log in to Docker Hub
+docker login -u priyeshshinav
+
+# Tag the Docker image
+docker tag priyeshshinav/public-dev-guvi:$1 priyeshshinav/public-dev-guvi:$1 
+
+# Push the Docker image to Docker Hub
+docker push priyeshshinav/public-dev-guvi:$1
